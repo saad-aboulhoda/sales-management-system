1f8ba0c459a1a82b94816079d9abfa59fb64c420
N1AKAI <rakansubs@gmail.com>
2024-11-24 12:07:15 +0100

    add setting's value to the view

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header class="app-header"><a class="app-header__logo" href="">Aya Chor</a>
+<header class="app-header"><a class="app-header__logo" href="">{{ $globalSettings->get('name') ?? 'Name' }}</a>
     <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar"
         aria-label="Hide Sidebar"></a>
     <!-- Navbar Right Menu-->
